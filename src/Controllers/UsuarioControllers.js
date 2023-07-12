c3b8dc16a71013fd1fa7c0ab5a220e3990e43886
Sbleit <zptrack24@gmail.com>
2023-07-11 21:23:02 -0400

    fix error addAnalytics

--- src/Controllers/UsuarioControllers.js
+++ src/Controllers/UsuarioControllers.js
@@ -1,5 +1,5 @@
 import { requestToBodyStream } from "next/dist/server/body-streams.js";
-import {Usuario, Analytics} from "../../mongo.js";
+import {Usuario, Analytics, Streaks} from "../../mongo.js";
 import en from "../DB/quotes/en.json" assert { type: "json"};
 import es from "../DB/quotes/es.json" assert { type: "json"};
 // import es from "../DB/quotes/es.json";
@@ -88,13 +88,54 @@ export const addAnalytics = async(req,res) => {
  console.log("body.createdAt",dato.createdAt)
  const fechaRecienteAnalitica = new Date(body.createdAt)
  const fechaUltimaAnalitica = new Date(dato.createdAt)
- console.log(fechaRecienteAnalitica)
- console.log(fechaUltimaAnalitica)
+ console.log("fechaRecienteAnalitica",fechaRecienteAnalitica)
+ console.log("fechaUltimaAnalitica",fechaUltimaAnalitica)
  if(fechaRecienteAnalitica > fechaUltimaAnalitica && fechaRecienteAnalitica.getDate() > fechaUltimaAnalitica.getDate())
  {
+  console.log("puedes completar todo")
   try{  
     const analytics = await Analytics(body);
-    const savedAnalytics = analytics.save();
+    const savedAnalytics = await analytics.save();
+    const racha = await Streaks.findOne({uid})
+    if(!racha){
+      const nuevaRacha = new Streaks({
+        uid,
+        days:0,
+        lastMaxStreak:0,
+        createdAt:body.createdAt,
+      });
+      nuevaRacha.save();
+      return res.status(200).send(nuevaRacha);
+    }
+    if(body.streak){
+      //no completo todo
+      //la racha se va
+      //si no tiene racha registrada, entonces se crea.
+      // if(!racha){
+      //   new Streaks({
+      //     uid,
+      //     days:0,
+      //     lastMaxStreak:0,
+      //     createdAt:body.createdAt,
+      //   });
+      // }
+      racha.days += 1;
+      racha.lastMaxStreak +=1;
+      await racha.save();
+    }else{
+      console.log("pero entro acá")
+      // if(!racha){
+      //   new Streaks({
+      //     uid,
+      //     days:0,
+      //     lastMaxStreak:0,
+      //     createdAt:body.createdAt,
+      //   });
+      // }
+      racha.days = 0;
+      await racha.save();
+    //caso contrario se reinicia la racha y se guarda su latests
+    }
     return res.status(200).send(savedAnalytics);
    }catch(err){
     return res.status(403).send({message:err.message});
