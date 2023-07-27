dfbde1f67956cab5b93c6e69ae0a4ee04d65eda9
Sbleit <72667973+SbleitZ@users.noreply.github.com>
2023-07-27 00:03:29 -0400

    Update Task.js

--- src/routes/Task.js
+++ src/routes/Task.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addTask, getTaskByUid, deleteById, editTaskById, completeEveryHabits, addAnalytics, getAnalyticsByUid, getStreaksByUid } from "../Controllers/UsuarioControllers.js";
+import { addTask, getTaskByUid, editTaskById, completeEveryHabits, addAnalytics, getAnalyticsByUid, getStreaksByUid,deleteEveryTaskById } from "../Controllers/UsuarioControllers.js";
 const router = express.Router();
 
 router.get('/:uid', getTaskByUid);
@@ -16,7 +16,8 @@ router.post('/confirm_complete/:uid',(req,res)=>{
   }
 });
 router.patch('/edit/:id',editTaskById);
-router.delete('/delete/:id/',deleteById);
+//router.delete('/delete/:id/',deleteById);
+router.delete('/delete/:id/',deleteEveryTaskById);
 // router.delete('/complete/:uid',completeEveryHabits);
 
 export default router;
